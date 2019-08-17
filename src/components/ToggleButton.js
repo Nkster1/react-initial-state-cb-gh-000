2beaa53ae40ae461704ba5c46c7a163ffb573aa0
N M <niclas@mettenleiter.com>
2019-08-17 10:14:35 +0000

    Automatically backed up by Learn

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,1 +1,12 @@
 // Component Code Goes Here
+import React from "react";
+
+class ToggleButton extends React.Component {
+  constructor () {
+    super();
+
+    this.state = {
+      isEnabled: false;
+    }
+  }
+}
