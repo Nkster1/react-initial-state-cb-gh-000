03fb58c29bd59b49a448f12d168df361cc4adcbc
N M <niclas@mettenleiter.com>
2019-08-17 10:20:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ToggleButton from "./components/ToggleButton"
+import ToggleButton from "./components/ToggleButton";
 
 ReactDOM.render(
   <div>Components Need To Be Mounted</div>,
